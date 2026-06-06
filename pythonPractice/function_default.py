3cc44f6b267f3b2960798284f091a6e3ff008536
Tabriji <tabrijiummi@gmail.com>
2026-06-06 12:55:18 +0600

    changes

---
+++ pythonPractice/function_default.py
@@ -0,0 +1,15 @@
+# def myfnc(x, y=10, z):
+#     print("x: ", x, "y: ", y, "z: ", z)
+
+# myfnc(1, 2, 3)
+
+
+def my_fnc(x, z, y=10):
+    print("x: ", x, "y: ", y, "z: ", z)
+
+
+my_fnc(1, 2, 3)
+a = 10
+b = 20
+c = 30
+my_fnc(y=a, z=b, x=c)
